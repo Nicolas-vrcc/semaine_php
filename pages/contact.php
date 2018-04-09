82c9e08eac648d882a5fa75df0b0609a6586cc2b
mattmlz <matthieu.toussaint@hetic.net>
2018-04-09 20:56:51 +0200

    resolving contact form bugs

--- pages/contact.php
+++ pages/contact.php
@@ -5,35 +5,33 @@ require_once '../includes/header.php';
 <div class="container contact_form">
   <div class="card-panel">
     <div class="row">
-      <form class="col s12">
+      <form class="col s12" method="post" action="/includes/send_email.php">
         <div class="row">
           <div class="input-field col s6">
             <i class="material-icons prefix">account_circle</i>
-            <input id="first_name" type="text" class="validate">
+            <input id="first_name" name="first_name" type="text" class="validate">
             <label for="first_name">Prénom</label>
           </div>
           <div class="input-field col s6">
-            <input id="last_name" type="text" class="validate">
+            <input id="last_name" name="last_name" type="text" class="validate">
             <label for="last_name">Nom</label>
           </div>
         </div>
         <div class="row">
           <div class="input-field col s12">
             <i class="material-icons prefix">email</i>
-            <input id="email" type="email" class="validate">
+            <input id="email" name="email" type="email" class="validate">
             <label for="email">Email</label>
           </div>
         </div>
         <div class="row">
           <div class="input-field col s12">
             <i class="material-icons prefix">mode_edit</i>
-            <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
-            <label for="icon_prefix2">Votre message</label>
+            <textarea id="message_content" name="message_content" class="materialize-textarea"></textarea>
+            <label for="message_content">Votre message</label>
           </div>
         </div>
-        <button class="btn waves-effect waves-light right" type="submit" name="action">Envoyer
-          <i class="material-icons right">send</i>
-        </button>
+        <button class="btn waves-effect waves-light btn-large" type="submit" name="Email" value="Email">Envoyer<i class="material-icons right">send</i></button>
       </form>
     </div>
   </div>
