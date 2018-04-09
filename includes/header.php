fcd8b727e6f622f2485e9d82644f0170f77bc174
mattmlz <matthieu.toussaint@hetic.net>
2018-04-09 14:51:59 +0200

    add basic footer

--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
     <link rel="stylesheet" href="./assets/styles/scss/index.css">
     <title>Super Voisin</title>
 </head>
 <body>
+
+<nav class="header_menu">
+  <div class="nav-wrapper">
+    <a href="#!" class="brand-logo">Super Voisin</a>
+    <ul class="right hide-on-med-and-down">
+      <li><a href="sass.html"><i class="material-icons right">account_circle</i>S'inscrire</a></li>
+      <li><a href="badges.html"><i class="material-icons right">vpn_key</i>Se connecter</a></li>
+    </ul>
+  </div>
+</nav>
