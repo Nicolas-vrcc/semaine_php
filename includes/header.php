acd958a1f14f12d81a32bb3e5aecd1aa570c4a4c
Unknown <matthieu.toussaint@hetic.net>
2018-04-09 17:40:48 +0200

    Bug of logo header & root correction

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
     <!-- Material icons  -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="./assets/styles/scss/index.css">
+    <link rel="stylesheet" href="/assets/styles/scss/index.css">
     <title>Super Voisin</title>
 </head>
 <body>
@@ -16,7 +16,7 @@
 <nav class="header_menu">
   <div class="container">
     <div class="nav-wrapper">
-      <a href="#!" class="brand-logo">Super Voisin<img src="" alt="logo header"></a>
+      <a href="#!" class="brand-logo">Super Voisin<img src="/assets/img/logo_header.svg" alt="logo header"></a>
       <ul class="right hide-on-med-and-down">
         <li><a href="badges.html"><i class="material-icons right">vpn_key</i>Se connecter</a></li>
         <li><a href="sass.html"><i class="material-icons right">account_circle</i>S'inscrire</a></li>
