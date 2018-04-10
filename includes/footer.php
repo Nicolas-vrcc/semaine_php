56e1ae24f37cfc49f6c3b480fe1356f7da1c621e
Unknown <matthieu.toussaint@hetic.net>
2018-04-10 11:14:21 +0200

    fix footer at the end of the page, instead of no content

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,5 @@
-  <!-- Compiled and minified JavaScript -->
+  </main>
+ <!-- Compiled and minified JavaScript -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
   <script src="/assets/js/main.js"></script>
 
