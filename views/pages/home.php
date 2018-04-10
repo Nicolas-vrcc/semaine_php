66465d2cc8dc31712d8ae06e1e601b278c253b4a
Unknown <benjaminhumblot@orange.fr>
2018-04-10 11:39:05 +0200

    Add homepage html in home.php

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,11 +1,78 @@
 <?php
 require_once 'views/includes/header.php';
 ?>
+<div class="connexion-panel">
 <div class="container">
-<div class="row">
-<h1>Home</h1>
+  <div class="row ">
+    <div class="col s12 l6 titleContainer">
+      <div class="title">Echange de services entre super-héros à paris</div>
+      <div class="description col  s10 l8"> Proposer des services qui entrent dans votre domaine de compétence auprès de tous les membres, et vous pouvez bénéficier de services dont vous avez vraiment besoin auprès de n’importe quel membre du site.</div>
+      <div class="col l6 offset-l12 supercoinman">
+        <img src="assets/img/supercoinman.svg" alt="supercoinman">
+      </div>
+    </div>
+   
+    <div class="col l4 buttonContainer">
+      <a class="waves-effect waves-light btn-large" href=""><p>Devenez un super héros</p></a>
+    </div>
+  </div>
 </div>
 </div>
+
+<div class="stepIndicator">
+<div class="container">
+  <div class="row">
+    <h2>Comment ça marche ?</h2>
+    <div class="col l4">
+      <h3>Inscription</h3>
+      <div class="stepNumber">1</div>
+      <div class="stepDescription">Inscrivez-vous et renseignez quelques informations pour pouvoir chercher des missions à accomplir.
+      </div>
+    </div>
+    <div class="col l4">
+      <h3>Comparer</h3>
+      <div class="stepNumber">2</div>
+      <div class="stepDescription">Créditez votre compteur de Super Coins  en répondant aux demandes des autres Super-héros et en leur rendant service
+      </div>
+    </div>
+    <div class="col l4">
+      <h3>Démarrer</h3>
+      <div class="stepNumber">3</div>
+      <div class="stepDescription">Demandez des services à d'autres Super-héros. C’est ça l'échange de services! 
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="exampleContainer">
+<div class="container">
+  <div class="row">
+    <h2>Des exemples de Super-Héros</h2>
+    <div class="col s12 m10 l6 profilContainer">
+      <div class="profilPicture"><img src="" alt="" ></div>
+      <div class="infoContainer">
+        <div class="location">Paris 18</div>
+        <div class="name">Claude Durluk</div>
+        <div class="skills">Garde d'enfant</div>
+        <div class="description">Dynamique, amusant et sérieux je peux garder vos enfants le soir après l’école.</div>
+      </div>
+    </div>
+    
+    <div class="col s12 m10 l6 profilContainer">
+      <div class="profilPicture"><img src="" alt="" ></div>
+      <div class="infoContainer">
+        <div class="location">Paris 18</div>
+        <div class="name">Juliette Pititou</div>
+        <div class="skills">Informatique / Art / Cours</div>
+        <div class="description">Etudiante en école de web je propose des cours sur photoshop et illustrator ainsi que la réalisation de vos logotypes et maquettes web.</div>
+      </div>
+    </div>
+    
+  </div>
+</div>
+</div>
+
 <?php
 require_once 'views/includes/footer.php';
 ?>
