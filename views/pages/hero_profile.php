a09d1fa4cfd83f1cf3f047d3f2559277ac2902b8
Unknown <matthieu.toussaint@hetic.net>
2018-04-10 17:27:54 +0200

    First section of hero profile

--- views/pages/hero_profile.php
+++ views/pages/hero_profile.php
@@ -1,2 +1,28 @@
 <?php
+require_once 'views/includes/db.php';
+require_once 'views/includes/header.php';
+?>
 
+<div class="informations">
+  <div class="container informations">
+    <div class="row">
+      <div class="col s4 profile_photo">
+        <div class="photo_container valign-wrapper">
+          <img src="/assets/img/marijooj.jpg" alt="profile picture">
+        </div>
+      </div>
+      <div class="col s8">
+        <p class="location">Paris 6e</p>
+        <p class="name">Jaaj Marijooj</p>
+        <p class="age">60 ans</p>
+        <p class="description">Je donne des cours d’anglais pour les tous les niveaux. Né en angleterre je suis complètement billingue.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<?php
+require_once 'views/includes/footer.php';
+?>
