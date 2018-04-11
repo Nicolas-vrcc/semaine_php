e8b3faf3f283800c82a466cccecc49a162b82489
Unknown <matthieu.toussaint@hetic.net>
2018-04-11 23:01:16 +0200

    edit profile

--- views/pages/dashboard/edit_profile.php
+++ views/pages/dashboard/edit_profile.php
@@ -12,47 +12,62 @@ echo '</pre>';
   <h1 class="center bangers">Modifiez votre profil de super-héros</h1>
 </div>
 
-<div class="container">
-  <div class="row form">
-  	<div class="col s3 photo_edit">
+<div class="container profile-details">
+  <div class="row valign-wrapper">
+  	<div class="col s4 photo_edit">
   	  <div class="photo_container_edit valign-wrapper">
-  	    <img class="responsive-img" src="/assets/img/marijooj.jpg" alt="profile picture">
+  	    <img class="responsive-img" src="<?= $_SESSION['auth']->avatar_picture; ?>" alt="profile picture">
   	  </div>
-  	</div>
-  	<div class="col s9 content_form">
-  	  <h3 class="sub_title bangers">Informations</h3>
 		</div>
-  	<form action="#" method="post" class="edit">
-  	  <div class="input-field col s4">
-  	  	<input id="name" type="text" class="validate">
-  	  	<label for="name">Nom</label>
-  	  </div>
-  	  <div class="input-field col s4 offset-s1">
-  	    <input id="first_name" type="text" class="validate">
-  	    <label for="first_name">Prénom</label>
-  	  </div>
-  	  <div class="input-field col s1">
-  	    <input id="age" type="number" class="validate">
-  	    <label for="age">Age</label>
-  	  </div>
-  	  <div class="input-field col s2 offset-s1">
-  	    <input id="location" type="number" class="validate">
-  	    <label for="location">Lieu</label>
-  	  </div>
-  	  <div class="col s9 content_form">
-  	    <h3 class="sub_title bangers">Super-pouvoirs</h3>
-  	  </div>
-  	  <div class="col s9 content_form offset-s3">
-  	    <h3 class="sub_title bangers">Description</h3>
-  	  </div>
-  	  <div class="input-field col s9 offset-s3">
-  	    <input id="description" type="text" class="validate">
-  	    <label for="description">Description</label>
-  	  </div>
-  	  <div class="input-field col s4 offset-s3">
-  	    <button class="btn btn_validate waves-effect waves-light" type="submit" name="action">Valider</button>
-  	  </div>
-  	</form>
+		
+		<div class="col s8 profile-informations">
+			<form action="#" method="post">
+				<h3 class="informations">Informations</h3>
+
+				<div class="row">
+					<div class="input-field col s6">
+        	  <input id="Prénom" type="text" class="validate" disabled>
+						<label for="Prénom"><?= $_SESSION['auth']->first_name;?></label>
+						Pour modifier votre prénom, veuillez nous contacter.
+					</div>
+					<div class="input-field col s6">
+        	  <input id="email" type="email" class="validate">
+        	  <label for="email"><?= $_SESSION['auth']->email; ?></label>
+        	</div>
+				</div>
+
+				<div class="row">
+					<div class="input-field col s12">
+						<input id="location" type="text" class="validate autocompleted">
+						<label for="location"><?= $_SESSION['auth']->location; ?></label>
+						Nous n'avons besoin que du nom de la rue et la ville.
+					</div>
+				</div>
+
+				<h3 class="biographie">Biographie</h3>
+				<div class="row">
+					<div class="input-field">
+						<textarea id="textarea1" class="materialize-textarea"></textarea>
+						<label for="textarea1"><?= $_SESSION['auth']->bio; ?></label>
+						Renseigner votre biographie permettra aux super-aidés de mieux vous connaître !
+					</div>
+				</div>
+
+				<h3 class="powers">Super-pouvoirs</h3>
+				<?php require_once 'views/includes/checkbox.php'; ?>
+				<div class="row">
+					<div class="col s12 select-powers center">
+						Sélectionnez vos super-pouvoirs !
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="col s12 center">
+						<button class="btn waves-effect waves-light center" type="submit" name="action">Modifier le profil</button>
+					</div>
+				</div>
+			</form>
+		</div>
   </div>
 </div>
 
