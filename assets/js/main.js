fd75d111618aeb3bbbad385674bd46d855f30df0
Unknown <matthieu.toussaint@hetic.net>
2018-04-11 19:23:04 +0200

    Add config to display the Google Maps with dynamic coordinates

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,31 +32,17 @@ numVisible:1
 });
 
 //Map in hero profile
-const latitude = coord[0]
-const longitude = coord[1]
+
 
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: latitude, lng: longitude},
-      zoom: 12
-    });
-
-    var drawingManager = new google.maps.drawing.DrawingManager({
-      drawingMode: google.maps.drawing.OverlayType.MARKER,
-      drawingControl: true,
-      drawingControlOptions: {
-        position: google.maps.ControlPosition.TOP_CENTER,
-        drawingModes: ['marker', 'circle', 'polygon', 'polyline', 'rectangle']
-      },
-      markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
-      circleOptions: {
-        fillColor: '#ffff00',
-        fillOpacity: 1,
-        strokeWeight: 5,
-        clickable: false,
-        editable: true,
-        zIndex: 1
-      }
-    });
-    drawingManager.setMap(map);
-  }
+	//Set latitude and longitude from hero_profile datas
+  const latitude = coord[0]
+  const longitude = coord[1]
+	const latlng = new google.maps.LatLng(latitude, longitude)
+	
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: latlng,
+    zoom: 12
+	});
+	
+}
