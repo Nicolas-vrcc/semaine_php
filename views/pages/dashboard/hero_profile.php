2bcd34effe1107361a449848f867c26719ecb57c
Unknown <matthieu.toussaint@hetic.net>
2018-04-11 19:43:06 +0200

    Remove price from hero profile page

--- views/pages/dashboard/hero_profile.php
+++ views/pages/dashboard/hero_profile.php
@@ -48,10 +48,6 @@ $coord = array($profil->latitude, $profil->longitude);
           <span class="skill"><?= $_skill ?></span>
         <?php endforeach ?>
       </div>
-      <div class="price center">
-        <p class="amount">30</p>
-        <img src="/assets/img/supercoin.svg" alt="Super Coin">
-      </div>
       <a href="mailto:<?= $profil->email?>"><button class="btn waves-effect waves-light" type="submit" name="action">Contacter</button></a>
     </div>
   </div>
