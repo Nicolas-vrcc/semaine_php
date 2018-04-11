68684c0423ac4a7159631c37c63df32c427adb00
Unknown <matthieu.toussaint@hetic.net>
2018-04-11 16:21:14 +0200

    Change call to Google Maps API

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -24,41 +24,8 @@
    <!-- Compiled and minified JavaScript -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
   <!-- Maps API -->
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDNj6Ao4vyLXmFpltuu8EnyYKYbF1HNXCM&libraries=places"></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDNj6Ao4vyLXmFpltuu8EnyYKYbF1HNXCM&libraries=places&libraries=drawing&callback=initMap" async defer></script>
 <!-- Main JS -->
 <script src="/assets/js/main.js"></script>
-<!-- Google Maps API -->
-<script>
-  // This example requires the Drawing library. Include the libraries=drawing
-  // parameter when you first load the API. For example:
-  // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=drawing">
-
-  function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
-    });
-
-    var drawingManager = new google.maps.drawing.DrawingManager({
-      drawingMode: google.maps.drawing.OverlayType.MARKER,
-      drawingControl: true,
-      drawingControlOptions: {
-        position: google.maps.ControlPosition.TOP_CENTER,
-        drawingModes: ['marker', 'circle', 'polygon', 'polyline', 'rectangle']
-      },
-      markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
-      circleOptions: {
-        fillColor: '#ffff00',
-        fillOpacity: 1,
-        strokeWeight: 5,
-        clickable: false,
-        editable: true,
-        zIndex: 1
-      }
-    });
-    drawingManager.setMap(map);
-  }
-</script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAMlTrWYLqu1qvbAN7q0S3lcWYpTlKXnQ4&libraries=drawing&callback=initMap" async defer></script>
 </body>
 </html>
