70a103916c3e9fad22e518af42d4bd9618c87bc5
Unknown <matthieu.toussaint@hetic.net>
2018-04-11 16:22:06 +0200

    Add script of google Maps API

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,30 @@ const instanceSlider = M.Carousel.init(slider, {
 indicators:true,
 numVisible:1
 });
+
+//Map in hero profile
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 48.856614, lng: 2.352222},
+      zoom: 12
+    });
+
+    var drawingManager = new google.maps.drawing.DrawingManager({
+      drawingMode: google.maps.drawing.OverlayType.MARKER,
+      drawingControl: true,
+      drawingControlOptions: {
+        position: google.maps.ControlPosition.TOP_CENTER,
+        drawingModes: ['marker', 'circle', 'polygon', 'polyline', 'rectangle']
+      },
+      markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
+      circleOptions: {
+        fillColor: '#ffff00',
+        fillOpacity: 1,
+        strokeWeight: 5,
+        clickable: false,
+        editable: true,
+        zIndex: 1
+      }
+    });
+    drawingManager.setMap(map);
+  }
