bb87a7bff3344efc46ecaa2e9924d3af0ddf0bae
Unknown <matthieu.toussaint@hetic.net>
2018-04-12 17:26:12 +0200

    checked attribute to skills editing profile w/ change inscription

--- views/includes/checkbox.php
+++ views/includes/checkbox.php
@@ -4,18 +4,21 @@ require_once 'views/includes/db.php';
 $query = $pdo->query('SELECT * FROM powers');
 $powers = $query->fetchAll();
 
-$skills = explode(',',$_SESSION['auth']->skills);
+//Do this only in the case of editing a profile
+if(isset($_SESSION)){
+	//Transform skills into an array
+	$skills = explode(',',$_SESSION['auth']->skills);
+	//Trim all the values in the array to prevent white spaces
+	$skill = array_map('trim', $skills);
+}
+
+foreach($powers as $_power):
+	//do not disturb the inscription page
+	if(isset($_SESSION)){
+		$attribute = in_array(trim($_power->name), $skill) ? 'checked' : '';
+	}
 ?>
 
-<?php foreach($powers as $_power): 
-/* 		if(in_array($_power->name, $skills)){
-			$attribute = 'checked';
-			continue;
-		} else {
-			$attribute = '';
-		} */
-	?>
-
-  <input id="<?= $_power->value ?>" class="checkbox" type="checkbox" name="arrayValue[]" value ="<?= $_power->name ?>">
+  <input id="<?= $_power->value ?>" class="checkbox" type="checkbox" name="arrayValue[]" value ="<?= $_power->name ?>" <? if(isset($attribute)){echo $attribute;} ?>>
   <label for="<?= $_power->value ?>" class="checklabel" ><?= utf8_decode($_power->name) ?></label>
 <?php endforeach; ?>
